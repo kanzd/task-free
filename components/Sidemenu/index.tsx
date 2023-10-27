19ce57e36c82322b696a4b591ac68d0cf39e6720
KanishkDixit <kanishk.dixit@go-yubi.com>
2023-10-27 21:49:18 +0530

    wintwealth task

---
+++ components/Sidemenu/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sidemenu() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
---
+++ styledcomponents/Grid.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-around;
+    align-item:center;
+`;
